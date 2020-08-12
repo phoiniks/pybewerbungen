699a26c0bfc374776a197d1b50f2e1d666fd7243
phoiniks <phoiniks@grellopolis.de>
2020-08-12 15:36:40 +0200

    gewünschte Funktionalität erreicht -- vorerst

--- bewerbungen.py
+++ bewerbungen.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
+from collections import UserDict
+import csv
+import locale
+import logging
+from pprint import pprint
 import sys
 from os.path import splitext, basename
-import logging
-import sqlite3
-from redis import Redis
-import csv
 import re
+import sqlite3
+from subprocess import check_output
 from time import strftime
-import locale
-from collections import UserDict
-from pprint import pprint
+
+from redis import Redis
 from jinja2 import Template, Environment, FileSystemLoader
 
 
@@ -21,7 +23,7 @@ logbuch = base + ".LOG"
 db = base + ".DB"
 csvDatei = base + ".CSV"
 
-logging.basicConfig(filename = logbuch, level = logging.DEBUG, format = '%(name)s %(asctime)s %(message)s %(funcName)s %(lineno)d')
+logging.basicConfig(filename = logbuch, filemode = 'w', level = logging.DEBUG, format = '%(name)s %(asctime)s %(message)s %(funcName)s %(lineno)d')
 log = logging.getLogger("DEBUG")
 
 locale.setlocale(locale.LC_ALL, '')
@@ -45,19 +47,20 @@ class Angebot(dict):
         self.ergebnis = ergebnis
         self.tex = self.firma + "_" + lokalzeit + ".tex"
         self.tex = re.sub(" ", "_", self.tex)
-
+        self.angebotstext = self.firma + "_" + lokalzeit + "_ANGEBOT.txt"
+        self.angebotstext = re.sub("\s+", "_", self.angebotstext)
         
     def mkSQLite(self):
         con = sqlite3.connect(db)
         cur = con.cursor()
         
         create = """CREATE TABLE IF NOT EXISTS angebote(id INTEGER PRIMARY KEY, bezeichnung TEXT, firma TEXT, strasse TEXT, ort TEXT,
-        anrede TEXT, ansprechpartner TEXT, mail TEXT, telefon TEXT, website TEXT, quelle TEXT, ergebnis TEXT, lokalzeit DATE DEFAULT(DATETIME('now', 'localtime')))"""
+        anrede TEXT, ansprechpartner TEXT, mail TEXT, telefon TEXT, website TEXT, quelle TEXT, ergebnis TEXT, angebotstext TEXT, lokalzeit DATE DEFAULT(DATETIME('now', 'localtime')))"""
 
         cur.execute(create)
 
-        insert = """INSERT INTO angebote (bezeichnung, firma, strasse, ort, anrede, ansprechpartner, mail, telefon, website, quelle, ergebnis)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+        insert = """INSERT INTO angebote (bezeichnung, firma, strasse, ort, anrede, ansprechpartner, mail, telefon, website, quelle, ergebnis, angebotstext)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 
         cur.execute(insert, (self.bezeichnung,
                              self.firma,
@@ -69,7 +72,8 @@ class Angebot(dict):
                              self.telefon,
                              self.website,
                              self.quelle,
-                             self.ergebnis))
+                             self.ergebnis,
+                             self.angebotstext))
 
         con.commit()
 
@@ -89,7 +93,7 @@ class Angebot(dict):
         
 
     def mkRedis(self):
-        dictionary = {
+        self.dictionary = {
             'bezeichnung':self.bezeichnung,
             'firma':self.firma,
             'strasse':self.strasse,
@@ -100,31 +104,56 @@ class Angebot(dict):
             'telefon':self.telefon,
             'website':self.website,
             'quelle':self.quelle,
-            'ergebnis':self.ergebnis}
+            'ergebnis':self.ergebnis,
+            'angebot':self.angebotstext}
 
-        with open("template.tex") as text:
+        self.rds = Redis()
+
+        for key in self.dictionary.keys():
+            self.rds.hset(self.tex, key, self.dictionary[key])
+
+
+    def mkText(self):
+        with open(sys.argv[1]) as text:
             templatestring = text.read()
+            log.debug("mkText_1")
+            log.debug(templatestring)
             template = Template(templatestring)
-
-        templatetext = template.render(dictionary)
-
+            log.debug("mkText_2")
+            log.debug("template")
+            
+        templatetext = template.render(self.dictionary)
+        log.debug("mkText3")
+        log.debug(templatetext)
+        
         templatetext = re.sub(" & Co.", " \& Co.", templatetext)
-
+        log.debug("mkText4")
+        log.debug(templatetext)
+        
         with open(self.tex, "w") as ausgabe:
+            log.debug("mkText5")
             ausgabe.write(templatetext)
+            log.debug("mkText6")
 
-        rds = Redis()
+        self.rds.hset(self.tex, 'anschreiben', templatetext)
+        
 
-        for key in dictionary.keys():
-            rds.hset(self.tex, key, dictionary[key])
-
-        rds.hset(self.tex, 'anschreiben', templatetext)
-
-
-log.debug("BEGINN")
+        with open(self.angebotstext, "w") as text:
+            angebot = check_output('xsel')
+            log.debug(angebot)
+            angebotstext = angebot.decode('utf-8')
+            log.debug(angebotstext)
+            
+            if len(angebotstext) == 0:
+                exit()
+                                                                                                    
+            text.write(angebotstext)
+            self.rds.hset(self.tex, self.angebotstext, angebotstext)
 
 
 if __name__ == "__main__":
+    log.debug("BEGINN")
+
     bezeichnung = input("BEZEICHNUNG: ")
     firma = input("FIRMA: ")
     strasse = input("STRASSE: ")
@@ -137,12 +166,14 @@ if __name__ == "__main__":
     quelle = input("QUELLE: ")
     ergebnis = input("ERGEBNIS: ")
 
-
     angebot = Angebot(bezeichnung, firma, strasse, ort, anrede, ansprechpartner, mail, telefon, website, quelle)
 
     angebot.mkSQLite()
     angebot.mkCSV()
     angebot.mkRedis()
+    log.debug("VOR ANGEBOT.MKTEXT")
+    angebot.mkText()
+    log.debug("NACH ANGEBOT.MKTEXT")
     
     log.debug(angebot.bezeichnung)
     log.debug(angebot.firma)
